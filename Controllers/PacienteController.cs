54639d64fba0ac4808becc8365936c695e98dbe9
Schulten <alejandro.schulten@accenture.com>
2020-01-02 17:52:40 -0300

    Creado de las controladoras, CRUD y filtrado de pacientes, creado Layout basico

---
+++ Controllers/PacienteController.cs
@@ -0,0 +1,143 @@
+﻿using SistemaDeTurnos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaDeTurnos.Controllers
+{
+    public class PacienteController : Controller
+    {
+        SystemaDeTurnosEntities db = new SystemaDeTurnosEntities();
+        // GET: Paciente
+        public ActionResult Index()
+        {
+            List<Paciente> listadoPacientes = db.Paciente.OrderBy(p => p.Apellido).ToList();
+            return View("Index",listadoPacientes);
+        }
+        [HttpPost]
+        public ActionResult Index(String aBuscar)
+        {
+            List<Paciente> todos = db.Paciente.ToList();
+            IQueryable<Paciente> qryNombre = db.Paciente;
+            IQueryable<Paciente> qryApellido = db.Paciente;
+            List<Paciente> listadoPacientes = new List<Paciente>();
+
+            if((aBuscar!= null) && (aBuscar.Length != 0))
+            {
+                qryNombre = qryNombre.Where(p => p.Nombre.Contains(aBuscar));
+                qryApellido = qryApellido.Where(p => p.Apellido.Contains(aBuscar));
+                foreach (Paciente paciente in qryNombre)
+                {
+                    listadoPacientes.Add(paciente);
+                }
+                foreach (Paciente pac in qryApellido)
+                {
+                    int flag = 0;
+                    foreach(Paciente p in listadoPacientes)
+                    {
+                        if(p.Id_Paciente == pac.Id_Paciente)
+                        {
+                            flag = 1;
+                        }
+                    }
+                    if (flag == 0)
+                    {
+                    listadoPacientes.Add(pac);
+                    }
+                }
+            
+
+            }
+
+            return View(listadoPacientes);
+        }
+        // GET: Paciente/Details/5
+        public ActionResult Details(int Id)
+        {
+            Paciente paciente = db.Paciente.Find(Id);
+            return View(paciente);
+        }
+        /*
+        public ActionResult Details(Paciente paciente)
+        {
+            return View();
+        }*/
+
+            // GET: Paciente/Create
+            public ActionResult Create()
+        {
+            this.ViewBag.TituloPagina = "Agregar Paciente";
+            Paciente paciente = new Paciente();
+            return View("Edit", paciente);
+        }
+
+        // POST: Paciente/Create
+        [HttpPost]
+        public ActionResult Create(Paciente paciente)
+        {
+            try
+            {
+                if (paciente == null)
+                {
+                    return Content("No puedo insertar los datos, faltan datos");
+                }
+
+                if (paciente.Nombre.Length == 0)
+                {
+                    return Content("No puedo insertar los datos, faltan datos");
+                }
+                paciente.Id_Paciente = db.Paciente.Count();//Asigno el ultimo id de paciente
+                db.Paciente.Add(paciente);// lo agrego y guardo los cambios
+                db.SaveChanges();
+                // creo una nueva ficha a la cual voy a asignarle al paciente
+                Ficha nuevaFicha = new Ficha();
+                nuevaFicha.Id_Paciente = paciente.Id_Paciente;
+                db.Ficha.Add(nuevaFicha);
+                db.SaveChanges();
+
+                return RedirectToAction("Edit", paciente.Id_Paciente);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Paciente/Edit/5
+        public ActionResult Edit(int Id)
+        {
+            this.ViewBag.TituloPagina = "Editar Paciente";
+            Paciente aEditar = db.Paciente.Find(Id);
+            return View(aEditar);
+        }
+
+        // POST: Paciente/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Paciente paciente)
+        {
+            try
+            {
+                this.db.Paciente.Attach(paciente);
+                this.db.Entry(paciente).State = System.Data.Entity.EntityState.Modified;
+                this.db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Paciente/Delete/5
+        public ActionResult Delete(int Id)
+        {
+            Paciente paciente = this.db.Paciente.FirstOrDefault(p => p.Id_Paciente == Id);
+            this.db.Paciente.Remove(paciente);
+            this.db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ SQLQuery.sql
@@ -0,0 +1,91 @@
+CREATE DATABASE SystemaDeTurnos;
+GO
+USE SystemaDeTurnos;
+GO
+
+CREATE TABLE Paciente
+(
+	Id_Paciente INT PRIMARY KEY IDENTITY(1,1),
+	Nombre VARCHAR(50) NOT NULL,
+	Apellido VARCHAR(50) NOT NULL,
+	Dni INT UNIQUE NOT NULL,
+	Fecha_Nac DATE,
+	Direccion VARCHAR(100),
+	Telefono VARCHAR(20),
+	Telefono_Contacto VARCHAR(20),
+	Obra_Social VARCHAR(50),
+	Numero_OS VARCHAR(30)
+)
+GO
+
+CREATE TABLE Medico
+(
+	Id_Medico INT PRIMARY KEY IDENTITY(1,1),
+	Nombre VARCHAR(50) NOT NULL,
+	Apellido VARCHAR(50) NOT NULL,
+	Dni INT UNIQUE NOT NULL,
+	Matricula INT UNIQUE NOT NULL,
+	Telefono VARCHAR(20)
+)
+GO
+
+CREATE TABLE Observacion
+(
+	Id_Observacion INT PRIMARY KEY IDENTITY(1,1),
+	Fecha DATE,
+	Id_Paciente INT NOT NULL,
+	Id_Medico INT NOT NULL,
+	Detalle VARCHAR(500),
+	CONSTRAINT FK_Observacion_Paciente
+		FOREIGN KEY (Id_Paciente) 
+		REFERENCES Paciente(Id_Paciente),
+	CONSTRAINT FK_Observacion_Medico
+		FOREIGN KEY (Id_Medico) 
+		REFERENCES Medico(Id_Medico)	
+)
+GO
+
+CREATE TABLE Ficha
+(
+	Id_Ficha INT PRIMARY KEY IDENTITY(1,1),
+	Id_Paciente INT NOT NULL,
+	Fecha_Ingreso DATE,
+	Diagnostico VARCHAR(200),
+	Antecedentes VARCHAR(200),
+	Contraindicaciones VARCHAR(200),
+	CONSTRAINT FK_Ficha_Paciente
+		FOREIGN KEY (Id_Paciente) 
+		REFERENCES Paciente(Id_Paciente)
+)
+GO
+
+CREATE TABLE Tratamiento
+(
+	Id_Tratamiento INT PRIMARY KEY IDENTITY(1,1),
+	Id_Paciente INT NOT NULL,
+	Id_Medico INT NOT NULL,
+	Fecha DATE,
+	Detalle VARCHAR(500),
+	CONSTRAINT FK_Tratamiento_Paciente
+		FOREIGN KEY (Id_Paciente) 
+		REFERENCES Paciente(Id_Paciente),
+	CONSTRAINT FK_Tratamiento_Medico
+		FOREIGN KEY (Id_Medico) 
+		REFERENCES Medico(Id_Medico)
+)
+GO
+
+CREATE TABLE Turno
+(
+	Id_Turno INT PRIMARY KEY IDENTITY(1,1),
+	Id_Paciente INT NOT NULL,
+	Id_Medico INT NOT NULL,
+	Fecha DATE
+	CONSTRAINT FK_Turno_Paciente
+		FOREIGN KEY (Id_Paciente) 
+		REFERENCES Paciente(Id_Paciente),
+	CONSTRAINT FK_Turno_Medico
+		FOREIGN KEY (Id_Medico) 
+		REFERENCES Medico(Id_Medico)
+)
+GO
